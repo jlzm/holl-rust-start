b9ceefe991d79c851e5162d592401b42a5f70690
John <31950551+jlzm@users.noreply.github.com>
2025-08-15 14:43:02 +0000

    feat(match): add nested structures match example.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
 fn main() {
-    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.")
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
         }
-        Message::Move { x, y } => {
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
             println!(
-                "Move in the x direction {} and in the y direction {}",
-                x,
-                y
-            );
-        }
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
-            println!(
-                "Change the color to red {}, green {}, and blue {}",
-                r,
-                g,
-                b
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
             )
         }
+        _ => (),
     }
 }
