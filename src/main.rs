f7d4e7e7aef3cd69dae60f64f2fb76db70683f64
John <31950551+jlzm@users.noreply.github.com>
2025-08-15 14:41:08 +0000

    feat(match): add match example.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
-    // 编译器自动推导出one的类型
-    let one = [1, 2, 3];
-    // 显式类型标注
-    let two: [u8; 3] = [1, 2, 3];
-    let blank1 = [0; 3];
-    let blank2: [u8; 3] = [0; 3];
+    let msg = Message::ChangeColor(0, 160, 255);
 
-    // arrays是一个二维数组，其中每一个元素都是一个数组，元素类型是[u8; 3]
-    let arrays: [[u8; 3]; 4] = [one, two, blank1, blank2];
-
-    // 借用arrays的元素用作循环中
-    for a in &arrays {
-        print!("{:?}: ", a);
-        // 将a变成一个迭代器，用于循环
-        // 你也可以直接用for n in a {}来进行循环
-        for n in a.iter() {
-            print!("\t{} + 10 = {}", n, n + 10);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
         }
-
-        let mut sum = 0;
-        // 0..a.len,是一个 Rust 的语法糖，其实就等于一个数组，元素是从0,1,2一直增加到到a.len-1
-        for i in 0..a.len() {
-            sum += a[i];
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y
+            );
         }
-        println!("\t({:?} = {})", a, sum);
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        }
     }
 }
