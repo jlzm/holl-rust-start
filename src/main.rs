56399ab83bce53ab4af4c7ead790e7188e5f8e33
John <31950551+jlzm@users.noreply.github.com>
2025-08-24 13:30:11 +0000

    feat(trait): [same fn] - add a example.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,34 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
-pub struct Post {
-    pub title: String,   // 标题
-    pub author: String,  // 作者
-    pub content: String, // 内容
+trait Pilot {
+    fn fly(&self);
 }
 
-impl Summary for Post {
-    fn summarize(&self) -> String {
-        format!("文章{}, 作者是{}", self.title, self.author)
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
     }
 }
 
-pub struct Weibo {
-    pub username: String,
-    pub content: String,
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
 }
 
-impl Summary for Weibo {
-    fn summarize(&self) -> String {
-        format!("{}发表了微博{}", self.username, self.content)
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
     }
 }
 
 fn main() {
-    let post = Post {
-        title: "Rust语言简介".to_string(),
-        author: "Sunface".to_string(),
-        content: "Rust棒极了!".to_string(),
-    };
-    let weibo = Weibo {
-        username: "sunface".to_string(),
-        content: "好像微博没Tweet好用".to_string(),
-    };
-
-    println!("{}", post.summarize());
-    println!("{}", weibo.summarize());
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+    Wizard::fly(&person);
 }
