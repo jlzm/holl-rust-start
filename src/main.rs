88a3e2fc55d4e396a81a6af6cddbf193fb5a9c6b
John <31950551+jlzm@users.noreply.github.com>
2025-09-06 15:22:11 +0000

    feat(start): add formatter example.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,33 @@ mod front_of_house {
     }
 }
 
+struct Person {
+    name: String,
+    age: u8,
+}
+
+use std::fmt;
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "hello i'm customize formatter: name-{}, age-{}",
+            self.name, self.age
+        )
+    }
+}
+
 fn main() {
     let lib_val = hosting::add_to_waitlist();
     let self_val = crate::front_of_house::hosting::add_to_waitlist();
     println!("lib val: {}", lib_val);
     println!("self val: {}", self_val);
+
+    let p_item = Person {
+        name: "xiaoyueyue".to_string(),
+        age: 7,
+    };
+
+    println!("{}", p_item);
 }
